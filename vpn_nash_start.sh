9177e939300ca57dcab8a3b5ade8813f0f9f883c
sunzhaohui <sunzhaohui@acbc327c45af.local>
2018-01-12 16:50:50 +0800

    add 2 file

---
+++ vpn_nash_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /usr/local/etc/openvpn/
+sudo /usr/local/sbin/openvpn --config /usr/local/etc/openvpn/rtr-yfc1xzjt.ovpn
---
+++ vpn_start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo /usr/local/sbin/openvpn --config /usr/local/etc/openvpn/client.ovpn
